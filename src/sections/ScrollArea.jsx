1f57523eadc59780743da4fe52ac616c6af9d36a
Julia Paul <juliapaulmyla@yahoo.com>
2024-02-12 23:00:07 -0500

    added animation and eslint

--- src/sections/ScrollArea.jsx
+++ src/sections/ScrollArea.jsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
 import { styled } from '@mui/system';
 
-const MyComponent = styled('aside')(({theme}) => ({
-  backgroundColor: theme.palette.primary.main,
-  width: '50vw',
-  height: '100vh',
-}));
 
 export default function ScrollArea() {
-  return <MyComponent>Styled div</MyComponent>;
+  const [coordX, setCoordX] = React.useState(null);
+  const [coordY, setCoordY] = React.useState(null);
+  const onHover = (event) => {
+    setCoordX(event.clientX);
+    setCoordY(event.clientY);
+  }
+
+  React.useEffect(() => {
+      window.addEventListener('mousemove', onHover);
+      return () => window.removeEventListener('mousemove', onHover);
+  })
+
+  const MyComponent = styled('aside')(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
+    width: '50vw',
+    height: '100vh',
+    '&:hover': {
+      backgroundImage: `radial-gradient(200px at ${coordX}px ${coordY}px, ${theme.palette.primary.light}, ${theme.palette.primary.main})`
+    }
+  }))
+  return <MyComponent 
+      coordX={coordX}
+      coordY={coordY}
+    >
+      Styled div
+    </MyComponent>;
 }
